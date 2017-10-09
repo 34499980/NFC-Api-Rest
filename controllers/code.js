4e9de3c669b8f8b2a0f6a7852ea06c0ea4110850
fedebucich <fedebucich@gmail.com>
2017-10-08 23:51:24 -0300

    code service improvements

--- controllers/code.js
+++ controllers/code.js
@@ -1,23 +1,25 @@
-var mongoose = require('mongoose');
-var Code  = mongoose.model('Code');
+var mongoose = require("mongoose");
+var Code = mongoose.model("Code");
 
 exports.getCode = function(req, res) {
-	Code.find(function(err, code) {
-    	if(err) res.send(500, err.message);
-    	console.log('GET /code')
-		res.status(200).jsonp(code);
-	});
+  Code.find(function(err, code) {
+    if (err) res.send(500, err.message);
+    console.log("GET /code");
+    res.status(200).jsonp(code);
+  });
 };
 
 exports.addCode = function(req, res) {
-	console.log('POST /code');
+  console.log("POST /code");
 
-	var code = new Code({
-		nfcTag: req.body.nfcTag				
-	});
+  var code = new Code({
+    nfcTag: req.body.nfcTag
+  });
 
-	code.save(function(err, code) {
-		if(err) return res.send(500, err.message);
-   		res.status(200).jsonp(code);
-	});
-}
+  Code.remove({}, function(err, codesObj) {
+    code.save(function(err, code) {
+      if (err) return res.send(500, err.message);
+      res.status(200).jsonp(code);
+    });
+  });
+};
