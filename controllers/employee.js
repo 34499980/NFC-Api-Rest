77955e162984e7316ca623b907edcd41aca90836
fedebucich <fedebucich@gmail.com>
2017-11-12 16:41:33 -0300

    not allowing repeated nfc tag or   expedient

--- controllers/employee.js
+++ controllers/employee.js
@@ -21,22 +21,14 @@ exports.addEmployee = function(req, res) {
   if (!validator.isAlphanumeric(req.body.expedient)) {res.status(500).send(String("El legajo debe contener letras y numeros unicamente."));return;}
   if (!validator.isAlpha(req.body.status)) {res.status(500).send(String("El estado debe contener solo letras."));return;}
 
-  //chequeo si ya existe un usuario con el mismo nombre apellido y legajo.
-  this.existEmployeeByFullNameAndExpedient(
-    {
-      name: req.body.name,
-      lastName: req.body.lastName,
-      expedient: req.body.expedient
-    },
+  //TODO investigar promises y ver si se puede mejorar usandolo.
+  this.duplicatedEmployee(
+    undefined,
+    { $or:[ {nfcTag: req.body.nfcTag}, {expedient: req.body.expedient} ]}, //La query que va a usar el find para ver si esta duplicado     
     function(err, exist) {
       if (exist) {
-        res
-          .status(500)
-          .send(
-            String(
-              "Ya exite un usuario con el mismo nombre, apellido y legajo."
-            )
-          );
+        res.status(500)
+           .send(String("Ya exite un usuario con el mismo legajo o numero de sube."));
       } else {
           const employee = new Employee({
             name: req.body.name,
@@ -60,18 +52,29 @@ exports.addEmployee = function(req, res) {
   );
 };
 
-existEmployeeByFullNameAndExpedient = function(req, result) {
-  Employee.findOne(
-    { name: req.name, lastName: req.lastName, expedient: req.expedient },
+duplicatedEmployee = function(employeeId, req, result) {
+  var context = this;
+  Employee.find(req,
     function(err, findEmp) {
       if (err) {
         console.log("error " + err);
         result(err);
       }
-      result(null, findEmp != null);
+      result(null, context.isOtherEmployee(findEmp, employeeId));
     }
   );
 };
+
+isOtherEmployee = function(empList, employeeId) {
+    if (empList == null) {
+      return false;
+    }
+    const l = empList.filter((e) => {
+      return e._id != employeeId;
+    });
+    return l.length != 0;
+};
+
 
 exports.updateEmployee = function(req, res) {
   if (validator.isEmpty(req.body.name) || validator.isEmpty(req.body.lastName)
@@ -82,30 +85,42 @@ exports.updateEmployee = function(req, res) {
   if (!validator.isAlphanumeric(req.body.expedient)) {res.status(500).send(String("El legajo debe contener letras y numeros unicamente."));return;}
   if (!validator.isAlpha(req.body.status)) {res.status(500).send(String("El estado debe contener solo letras."));return;}
 
-    Employee.findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        name: req.body.name,
-        lastName: req.body.lastName,
-        expedient: req.body.expedient,
-        nfcTag: req.body.nfcTag,
-        status: req.body.status,
-        scheduleWorkTime: req.body.scheduleWorkTime
-      },
-      { new: true },
-      function(err, updEmpl) {
-        if (err) res.status(500).send(String(err));
-
-        if (updEmpl == null) {
-          res
-            .status(404)
-            .send("No se econtro ningun empleado con el id : " + req.params.id);
-        } else {
-          saveAudit(2, updEmpl)
-          res.status(200).send(updEmpl);          
-        }
+  //TODO investigar promises y ver si se puede mejorar usandolo.
+  this.duplicatedEmployee(
+    req.params.id, 
+    {$or:[ {nfcTag: req.body.nfcTag}, {expedient: req.body.expedient} ]}, //La query que va a usar el find para ver si esta duplicado
+    function(err, exist) {
+      if (exist) { //Si encontro uno con el mismo legajo o sube que no sea el empleado mandado en el request
+        res.status(500)
+           .send(String("Ya exite un usuario con el mismo legajo o numero de sube."));
+      } else {
+        Employee.findOneAndUpdate(
+          { _id: req.params.id },
+          {
+            name: req.body.name,
+            lastName: req.body.lastName,
+            expedient: req.body.expedient,
+            nfcTag: req.body.nfcTag,
+            status: req.body.status,
+            scheduleWorkTime: req.body.scheduleWorkTime
+          },
+          { new: true },
+          function(err, updEmpl) {
+            if (err) res.status(500).send(String(err));
+    
+            if (updEmpl == null) {
+              res
+                .status(404)
+                .send("No se econtro ningun empleado con el id : " + req.params.id);
+            } else {
+              saveAudit(2, updEmpl)
+              res.status(200).send(updEmpl);          
+            }
+          }
+        );
       }
-    );
+    }
+  );
 };
 
 //change status to inactive to employee.
